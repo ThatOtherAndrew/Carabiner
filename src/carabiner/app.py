9ad26811606b6cf6fe021e31a7f420e48042a7a6
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-09-04 00:22:44 +0100

    Refactor logic into widget classes

--- src/carabiner/app.py
+++ src/carabiner/app.py
@@ -1,24 +1,43 @@
 import json
+import zipfile
 from os import getenv, PathLike
 from pathlib import Path
-from typing import ClassVar
+from typing import ClassVar, Any
 
+import yaml
 from textual import containers, widgets
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.widget import Widget
 
-from . import celeste
-
 
 class ModTile(containers.Horizontal):
-    def __init__(self, mod: celeste.Mod, *children: Widget):
-        self.mod = mod
+    def __init__(self, mod_zip_path: str | PathLike[str], *children: Widget):
         super().__init__(*children)
+
+        self.path = Path(mod_zip_path).resolve()
+
+        manifest = self._get_mod_manifest()
+        self.mod_name: str = manifest['Name']
+        self.mod_version = str(manifest['Version'])
+
+    def _get_mod_manifest(self) -> dict[str, Any]:
+        path = zipfile.Path(self.path)
+        if (path / 'everest.yaml').is_file():
+            yaml_path = path / 'everest.yaml'
+        elif (path / 'everest.yml').is_file():
+            yaml_path = path / 'everest.yml'
+        else:
+            raise FileNotFoundError('everest.y[a]ml file not found')
+
+        with yaml_path.open(encoding='utf-8-sig') as file:
+            manifest_data = yaml.safe_load(file)[0]
+
+        return manifest_data
 
     def compose(self) -> ComposeResult:
         yield widgets.Switch(animate=False)
-        yield widgets.Static(f'[green]{self.mod.name}[/] [dim]{self.mod.version}')
+        yield widgets.Static(f'[green]{self.mod_name}[/] [dim]{self.mod_version}')
 
     def on_click(self) -> None:
         self.query_one(widgets.Switch).toggle()
@@ -35,7 +54,7 @@ class CelesteInstance(containers.VerticalScroll):
         with widgets.Collapsible(title='Mods', collapsed=False):
             with containers.Grid():
                 for mod in (self.path / 'Mods').glob('*.zip'):
-                    yield ModTile(mod=celeste.Mod.from_zip(mod))
+                    yield ModTile(mod)
 
 
 class Carabiner(App):
